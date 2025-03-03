bcbd2eacb47fa1e8f88f2b53242a9e89b2a53471
Viki <hi@viki.moe>
2025-03-03 12:55:48 +0800

    fix: fix error tip data

--- debug/index.ts
+++ debug/index.ts
@@ -1,6 +1,6 @@
 import { paseArticleUrl } from '../parse-article-url.ts'
 
 const url =
-  'http://mp.weixin.qq.com/s?__biz=MzU2MDU4NDE1MQ==&mid=2247528254&idx=1&sn=2a94b1bc766fce1e776d8e20f6f672c0'
+  'http://mp.weixin.qq.com/s?__biz=MzU2MDU4NDE1MQ==&mid=2247528487&idx=1&sn=a5e53120fec7e0518f7d7b994a36b186'
 
 console.log(paseArticleUrl(url))
