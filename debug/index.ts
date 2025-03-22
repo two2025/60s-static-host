175aef4643a19983d701a74cf13448925425aaf3
Viki <hi@viki.moe>
2025-03-22 11:52:48 +0800

    chore: add 2025-03-22

--- debug/index.ts
+++ debug/index.ts
@@ -1,6 +1,6 @@
 import { paseArticleUrl } from '../src/parse-article-url.ts'
 
 const url =
-  'http://mp.weixin.qq.com/s?__biz=MzA5MTczNDc1OQ==&mid=2247495053&idx=1&sn=51a92780d46d2b352c99e33bb2351683'
+  'https://mp.weixin.qq.com/s/IbdhqVOt2hB5CBUcbn1Mhg'
 
 console.log(paseArticleUrl(url))
